3764181adbe0bac8b984a2e7e0195394b9a38465
varun-m <47769939+varun-m@users.noreply.github.com>
2020-01-13 10:08:12 +0300

    Initial commit

---
+++ Udemy/src/Base.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Base {
+	
+	public static AppiumDriver<MobileElement> capability() throws MalformedURLException {
+		
+		//Appium code
+		
+		File path=new File("src/app");
+		File appPath=new File(path,"ApiDemos-debug.apk");
+		
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+		cap.setCapability(MobileCapabilityType.APP, appPath.getAbsolutePath());
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
+//		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		
+		AppiumDriver<MobileElement> driver = new AppiumDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		return driver;
+	}
+}
---
+++ Udemy/src/BaseTest.java
@@ -0,0 +1,20 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+
+public class BaseTest extends Base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		AppiumDriver<MobileElement> driver = capability();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
+
+	}
+
+}
